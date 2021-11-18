16ecb030c10c173638cdc76ce803dd39e4cd629f
Felipepereiralima <felipeplima@live.com>
2021-11-18 16:20:26 -0300

    Adicionando Heroku e Swagger

--- BlogPessoal/src/main/java/br/com/generation/blogpessoal/seguranca/BasicSecurityConfig.java
+++ BlogPessoal/src/main/java/br/com/generation/blogpessoal/seguranca/BasicSecurityConfig.java
@@ -3,6 +3,7 @@ package br.com.generation.blogpessoal.seguranca;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -35,14 +36,16 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-			.antMatchers("/usuarios/logar").permitAll()
-			.antMatchers("/usuarios/cadastrar").permitAll()
-			.anyRequest().authenticated()
-			.and().httpBasic()
-			.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-			.and().cors()
-			.and().csrf().disable();
+	protected  void  configure(HttpSecurity  http) throws  Exception {
+
+	http.authorizeRequests()
+		.antMatchers("/usuarios/cadastrar").permitAll()
+		.antMatchers("/usuarios/logar").permitAll()
+		.antMatchers(HttpMethod.OPTIONS).permitAll()
+		.anyRequest().authenticated()
+		.and().httpBasic()
+	    .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and().cors()
+		.and().csrf().disable();
 	}
 }
